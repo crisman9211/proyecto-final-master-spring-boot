2b3b97f73d7f25969e134cdde44b0f1514486b29
Cristian Alfredo Manzo Parra <ca.manzo973@uniandes.edu.co>
2024-05-12 02:32:59 -0500

    add user service

---
+++ microservice-user-management/src/test/java/com/crisman9211/microserviceusermanagement/MicroserviceUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisman9211.microserviceusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
